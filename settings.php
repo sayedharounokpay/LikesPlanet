ff4d108d5fb997b82834e7b71a0adaa9b70ace04
Richard Abear <owchzzz@gmail.com>
2014-11-04 16:36:22 +0800

    fixed untracked files

--- settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host		= "localhost"; 	// "127.0.0.1" ;  // your mysql server address  localhost
-$user		=  "likespla_liker"; 	// "root" ; // your mysql username
-$pass		= "eaK}WdQ@q#~D";	//"" ; // your mysql password
-$tablename	=	"likespla_ankur625"; // your database name
-?> 
