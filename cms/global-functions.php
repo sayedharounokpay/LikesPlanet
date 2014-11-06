5656a402136282010d0a1a0e5442814931117448
Richard Abear <owchzzz@gmail.com>
2014-11-06 19:32:15 +0800

    Global Functionality Changes (CMS)

--- cms/global-functions.php
+++ cms/global-functions.php
@@ -92,7 +92,7 @@ class dbTable {
         }
         $query = "SELECT * FROM ".$this->table." LIMIT $offset,$limit";
         if(strlen($where) > 5 && ($blank < $wherecount)) {
-            $query = "SELECT * FROM ".$this->table." WHERE $where LIMIT 1";
+            $query = "SELECT * FROM ".$this->table." WHERE $where";
         }
        
       
