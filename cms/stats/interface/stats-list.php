d001d6aa11cd4b1aa22ecbc198df1d19fd6816fb
Richard Abear <owchzzz@gmail.com>
2014-11-06 19:29:37 +0800

    Statistics Changes 1.1

--- cms/stats/interface/stats-list.php
+++ cms/stats/interface/stats-list.php
@@ -7,7 +7,7 @@ $pagenum = 1;
 if(isset($_GET['pagenum'])) {
     $pagenum = $_GET['pagenum'];
 }
-$usertable = new dbTable($table, $cols, $limit, $pagenum, $user_options = 1, '<a href="'.$baselocation.'/users/base.php?action=uoptions&id=.id.">User Options</a>');
+$usertable = new dbTable($table, $cols, $limit, $pagenum, $user_options = 1);
 $usertable->display_search(array('user_id'=>'User ID','date'=>'Date'));
 $usertable->display_table();
 $usertable->pagination();
