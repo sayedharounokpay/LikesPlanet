8fb9457b85ff00f8bbcf2f726c0d5b8e925b0c88
Richard Abear <owchzzz@gmail.com>
2014-11-12 11:19:31 +0800

    Bug Tracing

--- fbstartlikeref.php
+++ fbstartlikeref.php
@@ -59,11 +59,11 @@ $mobilelink = $site->facebook;
 }
 echo $mobilelink;
 $graphlink = "";
-if(strpos($mobilelink,'http://www.facebook.com/') == TRUE) {
+if(strpos($mobilelink,'http://www.facebook.com/') !== false) {
     echo 'Found http://facebook';
     $graphlink = str_replace('http://www.facebook.com/', '', $mobilelink);
 }
-if(strpos($mobilelink,'https://www.facebook.com/') == TRUE) {
+if(strpos($mobilelink,'https://www.facebook.com/') !== false) {
     echo 'Found https://facebook';
     $graphlink = str_replace('https://www.facebook.com/', '', $mobilelink);
 }
