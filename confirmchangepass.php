e2f44664a249360a050ac442c078be6aae54ff26
Richard Abear <owchzzz@gmail.com>
2014-11-14 15:59:19 +0800

    Password Recovery Changes

--- confirmchangepass.php
+++ confirmchangepass.php
@@ -1,16 +1,18 @@
 <?php
+
 $message = "";
 include('config.php');
+error_reporting(E_ALL);
 if(isset($_POST['requestid'])) {
     if(isset($_POST['conf']) && (strlen($_POST['conf']) > 5)) {
         if($passwordcheck = mysql_query("SELECT * FROM pass_requests WHERE id={$_POST['requestid']} AND newpass='{$_POST['conf']}' AND completed=0")){
             if($request = mysql_fetch_object($passwordcheck)) {
                 if(isset($_POST['pass']) && strlen($_POST['pass']) > 5) {
                     if(isset($_POST['repass']) && $_POST['pass'] == $_POST['repass']) {
-                        mysql_query("UPDATE pass_requests WHERE id={$_POST['requestid']} SET completed=1");
+                        mysql_query("UPDATE pass_requests SET completed=1 WHERE id={$_POST['requestid']} ");
                         $userid = $request->user;
                         $newpass = md5($_POST['pass']);
-                        mysql_query("UPDATE users WHERE login='{$userid}' SET pass='{$newpass}'");
+                        mysql_query("UPDATE users SET pass='{$newpass}' WHERE login='{$userid}' ");
                         $message = "Password Has successfully Been changed. Please proceed to <a href=\"http://www.likesplanet.com/login.php\">Login</a>";
                     }
                     else {
