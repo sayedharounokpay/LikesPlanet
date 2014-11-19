3fc0e71381820e02d6bd9ff73acea9723361ad34
Richard Abear <owchzzz@gmail.com>
2014-11-19 15:46:50 +0800

    Paypal IPN

--- process_payment.php
+++ process_payment.php
@@ -34,7 +34,7 @@ if(isset($_GET['transactid'])) {
                 if($_POST['verify_sign'] != "") {
                     if($transaction->points == $_GET['pnt']) {
                         mysql_query("UPDATE `users` SET `coins`=`coins`+'{$_GET['pnt']}', `pr`=`pr`+'{$_GET['pnt']}', `bought`= '1' WHERE `id`='{$_GET['uid']}'");
-                        mysql_query("UPDATE transactions SET valid=1 WHERE transacid='".$_GET['transactid']."'")
+                        mysql_query("UPDATE transactions SET valid=1 WHERE transacid='".$_GET['transactid']."'");
                         mysql_query("INSERT INTO statistics (user_id,date,coins_gained,payment,log,page) VALUES ({$_GET['uid']},NOW(),{$_GET['pnt']},1,'<b style=\"color:green;\">Bought Points (Paypal)</b>','process_payment.php')");
                         mysql_query("INSERT INTO `orders` (user_id, points, date, price) VALUES('{$_GET['uid']}', '{$_GET['pnt']}', NOW(), '{$_GET["price"]}' ) ");
                     }
