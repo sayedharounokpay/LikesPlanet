84e079ec363bd7735d96a1b4b0f6a3956ef7fcdf
Richard Abear <owchzzz@gmail.com>
2014-11-19 15:36:06 +0800

    Paypal IPN

--- process_payment.php
+++ process_payment.php
@@ -27,10 +27,19 @@ $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 mail($to, $subject, $message, $headers);
 
 if(isset($_GET['transactid'])) {
-    $results = mysql_query("SELECT * FROM transactions WHERE transacid='".$_GET['transactid']."'");
+    $results = mysql_query("SELECT * FROM transactions WHERE transacid='".$_GET['transactid']."' AND valid=0");
     if($transaction = mysql_fetch_object($results)) {
         if($_POST['payment_status'] == 'Completed') {
-            mysql_query("UPDATE transactions SET valid=1 WHERE transacid='".$_GET['transactid']."'");
+            if($transaction->cash == $_POST['mc_gross'] || $transaction->cash == $_POST['payment_gross']) {
+                if($_POST['verify_sign'] != "") {
+                    if($transaction->points == $_GET['pnt']) {
+                        mysql_query("UPDATE `users` SET `coins`=`coins`+'{$_GET['pnt']}', `pr`=`pr`+'{$_GET['pnt']}', `bought`= '1' WHERE `id`='{$_GET['uid']}'");
+                        mysql_query("UPDATE transactions SET valid=1 WHERE transacid='".$_GET['transactid']."'")
+                        mysql_query("INSERT INTO statistics (user_id,date,coins_gained,payment,log,page) VALUES ({$_GET['uid']},NOW(),{$_GET['pnt']},1,'<b style=\"color:green;\">Bought Points (Paypal)</b>','process_payment.php')");
+                        mysql_query("INSERT INTO `orders` (user_id, points, date, price) VALUES('{$_GET['uid']}', '{$_GET['pnt']}', NOW(), '{$_GET["price"]}' ) ");
+                    }
+                }
+            }
         }
     }
 }
