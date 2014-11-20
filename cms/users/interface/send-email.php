37da8e42b88863ea1554ed973ea568e1b7a5b42b
Richard Abear <owchzzz@gmail.com>
2014-11-20 12:39:44 +0800

    Mass email sending

--- cms/users/interface/send-email.php
+++ cms/users/interface/send-email.php
@@ -3,7 +3,7 @@
 <script src="//tinymce.cachefly.net/4.1/tinymce.min.js"></script>
 <script>tinymce.init({selector:'textarea'});</script>
 <div class="row">
-<form class="form-horizontal" action="<?=$baselocation?>/users/email-send-act" method="POST">
+<form class="form-horizontal" action="<?=$baselocation?>/users/base.php?action=email-send-act" method="POST">
     <div class="form-group">
         <input type="text" class="form-control" name="subject" placeholder="Subject"/>
     </div>
@@ -12,5 +12,6 @@
             
         </textarea>
     </div>
+    <input type="submit" class="btn btn-lg btn-default" value="Submit"/>
 </form>
 </div>
