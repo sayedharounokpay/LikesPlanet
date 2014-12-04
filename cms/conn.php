3e680863c9dc2fada1fafd36f1a49dcc16ba2b3b
Richard Abear <owchzzz@gmail.com>
2014-12-04 17:15:35 +0800

    gitfix

--- cms/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "likespla_liker";
-$pass = "eaK}WdQ@q#~D";
-$db = "likespla_ankur625";
-
-$db = mysqli_connect($host, $user, $pass, $db) or die("Error " . mysqli_error($link));
-$link = mysql_connect('localhost', $user, $pass);
-$baselocation = "http://www.likesplanet.com/cms";
