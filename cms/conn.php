2b5472f3c8c739b7a5d2c68088437a64e76a3f11
Richard Abear <owchzzz@gmail.com>
2014-12-04 16:37:12 +0800

    CMS Fix

--- cms/conn.php
+++ cms/conn.php
@@ -1,9 +1,9 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
+$user = "likespla_liker";
+$pass = "eaK}WdQ@q#~D";
 $db = "likespla_ankur625";
 
 $db = mysqli_connect($host, $user, $pass, $db) or die("Error " . mysqli_error($link));
 $link = mysql_connect('localhost', $user, $pass);
-$baselocation = "http://localhost/LikesPlanet/cms";
+$baselocation = "http://www.likesplanet.com/cms";
